4a05cd6b4a028cef480290de5614c03d28bf6695
Spencer Hodson <spencer.s.hodson@gmail.com>
2025-12-05 14:19:19 -0700

    updated from email

--- requests/SendEmailRequest.ts
+++ requests/SendEmailRequest.ts
@@ -5,7 +5,7 @@ export type EmailBodyContent = ContactFormData;
 export class SendEmailRequest {
   // TODO: update the to email and from email with a domain that belongs to marc
   public to: string = "drmarclewis@gmail.com";
-  public from: string = "noreply@resend.dev";
+  public from: string = "noreply@drmarclewis.com";
   public subject: string;
   public body: EmailBodyContent;
 
